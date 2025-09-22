3ddeb4a5d4b4d99092bf8bb5f05d3b87f9cfbd16
Subhamay Aditya <aditya.subhamay@gmail.com>
2025-09-22 23:05:16 +0530

    web.php updated

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-
-return view('listings',[
-    'listing'=>Listing::all()
-]
-);
-
+    return view(
+        'listings',
+        [
+            'listing' => Listing::all()
+        ]
+    );
 });
 
 //Single post
 
 Route::get('/listings/{id}', function ($id) {
-    return view('listing',[
-    'listing'=>Listing::find($id)
-]
-);
+
+$listing= Listing::find($id);
+
+
+if($listing){
+  return view('listing',[
+            'listing' => Listing::find($id)
+        ]
+    );  
+}
+else{
+    abort('404');
+}
+
+    // return view('listing',[
+    //         'listing' => Listing::find($id)
+    //     ]
+    // );
 });

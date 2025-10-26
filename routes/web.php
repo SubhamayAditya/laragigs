0127968323dc802e084b51f3797b07775e83476f
Subhamay Aditya <aditya.subhamay@gmail.com>
2025-10-26 21:07:40 +0530

    middleware authentication added

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,19 @@ use App\Http\Controllers\UserController;
 Route::get('/', [ListingController::class,'index']);
 
 //Show create form
-Route::get('/listings/create', [ListingController::class, 'create'])->name('create');
+Route::get('/listings/create', [ListingController::class, 'create'])->name('create')->middleware('auth');
 
 //Store post
 Route::post('/listings', [ListingController::class, 'store']);
 
 //Show Edit Form
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->name('edit');
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->name('edit')->middleware('auth');
 
 //Update
 Route::put('/listings/{listing}', [ListingController::class, 'update'])->name('update');
 
 //Delete
-Route::delete('/listings/{listing}', [ListingController::class, 'delete'])->name('delete');
+Route::delete('/listings/{listing}', [ListingController::class, 'delete'])->name('delete')->middleware('auth');
 
 //Single post
 // Route::get('/listings/{id}',[ListingController::class,'show'] );
@@ -34,16 +34,16 @@ Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
 //Show register form
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
 //Create new user
 Route::post('/users', [UserController::class, 'store']);
 
 //Logut User
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
 
 //Show Login form
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login'])->name('login');
 
 //Login user
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
